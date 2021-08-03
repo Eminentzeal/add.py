a78b14f24587d852ce134a00bd16038663838c16
Ezekiel Ogunniwa <72137180+Eminentzeal@users.noreply.github.com>
2021-08-03 13:54:27 +0000

    Initial commit

---
+++ add.py
@@ -0,0 +1,7 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+
+sum = a + b + c
+
+print("The sum of the three number is:", sum)
